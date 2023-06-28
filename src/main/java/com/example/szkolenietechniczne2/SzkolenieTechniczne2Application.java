be8ed7079c9e424626d1dd7f749b288738c080a1
Wojturski Maciej <w65578@student.wsiz.edu.pl>
2023-06-28 13:05:23 +0200

    Again Turn On Spring Security

--- src/main/java/com/example/szkolenietechniczne2/SzkolenieTechniczne2Application.java
+++ src/main/java/com/example/szkolenietechniczne2/SzkolenieTechniczne2Application.java
@@ -3,9 +3,7 @@ package com.example.szkolenietechniczne2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,}
-)
+@SpringBootApplication
 public class SzkolenieTechniczne2Application {
     public static void main(String[] args) {
         SpringApplication.run(SzkolenieTechniczne2Application.class, args);
